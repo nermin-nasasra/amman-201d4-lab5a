18066b4d316730cec3fe6ce14e86199b8d242407
nermin-nasasra <nermeenmanaseer1496@yahoo.com>
2020-02-20 15:38:33 +0200

    mullarry function done

--- app.js
+++ app.js
@@ -125,13 +125,22 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
+
 function multiplyArray(multArr) { //eslint-disable-line
 
-}
+    var num2=1;
+    for(var i=0; i<testArray.length; i++)
+    
+    {
+        
+  num2=multiply(num2, testArray[i])[0];
+ }
+ var massage5=`The numbers ${testArray[0]},${testArray[1]},${testArray[2]} have a product of ${num2}.`;
 
-// Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+ return[num2, massage5];
 
+ }
+ testMultiplyArray(testArray);
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
 // You're done! Submit the link to the repo following the instructions in Canvas. Or, try out the stretch goal below...
