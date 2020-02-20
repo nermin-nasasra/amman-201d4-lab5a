ee860cea3638adaa69cae706cbf641d942f93bdc
nermin-nasasra <nermeenmanaseer1496@yahoo.com>
2020-02-20 15:12:50 +0200

    sumarry function done

--- app.js
+++ app.js
@@ -92,12 +92,25 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
+    var num=0;
+    for(var i=0; i<testArray.length; i++)
+    
+    {
+        
+  num=sum(num, testArray[i])[0];
+    }
+ var massage4=`${testArray[0]},${testArray[1]},${testArray[2]} was passed in as an array of numbers, and ${num} is their sum.`;
 
-}
+ return[num, massage4];
+
+ }
+//console.log(testSumArray());
+
+// testSumArray();
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
